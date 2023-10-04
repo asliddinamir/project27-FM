ee0dd1e68212e121654079dfec03533ba4ac8f13
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-04 18:25:14 +0800

    done js part

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const tipNum = document.querySelector('.tip_num');
 const totalNum = document.querySelector('.total_num');
 const resetBtn = document.querySelector('#resetBtn');
 const buttons = document.querySelectorAll('.buttons button');
+const customInput = document.querySelector('.custom');
 
 let selectedTip = 0;
 let billInputNum = 0;
-let peopleInputNum = 0
+let peopleInputNum = 0;
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -19,9 +20,32 @@ buttons.forEach(button => {
         // Add the "active" class to the clicked button
         button.classList.add('active');
         selectedTip = parseFloat(e.target.value);
+        customInput.value = '';
         calculateTip()
     });
 });
+
+customInput.addEventListener('input', (e) => {
+    let customTip = parseFloat(e.target.value);
+    if (isNaN(customTip)) {
+        customTip = 0;
+    }
+    if (customTip < 0) {
+        customTip = 0;
+    } else if (customTip > 100) {
+        customTip = 100;
+    }
+
+    customInput.value = customTip;
+
+    selectedTip = customTip;
+
+    selectedTip = parseFloat(e.target.value);
+    buttons.forEach(otherBtn => {
+        otherBtn.classList.remove('active');
+    });
+    calculateTip()
+})
 
 
 billInput.addEventListener('change', (e) => {
@@ -45,6 +69,7 @@ resetBtn.addEventListener('click', () => {
         button.classList.remove('active');
     });
 
+    customInput.value = '';
     billInput.value = '';
     peopleInput.value = '';
     tipNum.textContent = '$0.00';
